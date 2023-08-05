50bb27ea04dbfe7f31326b1ae2ea7d17ea45be3b
Tanne <quachtienthanh92@gmail.com>
2023-08-05 15:56:04 +0200

    test 2

--- my-app/src/components/Footer/Footercontent.js
+++ my-app/src/components/Footer/Footercontent.js
@@ -5,15 +5,15 @@ import SynologenImg from "../Card UI/Synologen";
 const FooterBlock = () => {
     return (
         <div className=" relative  ">
-            <div className=" absolute z-[999] max-h-[80%]  grid grid-cols-2 grid-rows-3  gap-x-8  px-[30px]  inset-0  mt-[60px]">
+            <div className=" absolute z-[999] h-[90%]   grid grid-cols-2 grid-rows-3  gap-x-8  px-[30px]  inset-0  mt-[20px]">
                 {/* Info1 */}
 
-                <div>
+                <div className="leading-7">
                     <h3 className="text-[1.2rem] mb-[1rem] font-bold font-Exo text-white">
                         Linser & Glasögon
                     </h3>
                     <ul>
-                        <li className="text-white capitalize">Kontaklinser</li>
+                        <li className="text-white  capitalize">Kontaklinser</li>
                         <li className="text-white capitalize">
                             Arbetsglasögon
                         </li>
@@ -24,7 +24,7 @@ const FooterBlock = () => {
                         <li className="text-white capitalize">Solglasögon</li>
                     </ul>
                 </div>
-                <div className="">
+                <div className="leading-9">
                     <h3 className="text-[1.2rem] mb-[1rem] font-bold font-Exo text-white">
                         Synundersökning
                     </h3>
@@ -34,7 +34,7 @@ const FooterBlock = () => {
                 {/* Info2 */}
 
                 <div>
-                    <div className=" ">
+                    <div className=" mt-[20px] leading-7 ">
                         <h3 className="text-[1.2rem] mb-[1rem] font-bold font-Exo text-white">
                             Information
                         </h3>
@@ -60,7 +60,7 @@ const FooterBlock = () => {
                         </span>
                     </div>
                 </div>
-                <div>
+                <div className="mt-[20px]">
                     <span className="text-20  font-Exo font-bold text-white uppercase">
                         Följ oss
                     </span>
@@ -79,7 +79,7 @@ const FooterBlock = () => {
                         </a>
                     </div>
                 </div>
-                <div className="relative flex flex-col items-center translate-y-[-10%] justify-center col-span-2 ">
+                <div className="relative flex flex-col items-center translate-y-[25%] justify-center col-span-2 ">
                     <SynologenImg></SynologenImg>
                     <span className="  font-Exo  text-white mt-[1rem] ">
                         Kvlaitéts & Miöjöpolicy

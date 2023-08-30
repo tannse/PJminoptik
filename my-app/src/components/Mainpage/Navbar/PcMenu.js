f58b48f6aa3b8724209bbec9d3fdd24f9a10373d
Tanne <quachtienthanh92@gmail.com>
2023-08-30 12:29:28 +0200

    fix bug duration time on Navbar

--- my-app/src/components/Mainpage/Navbar/PcMenu.js
+++ my-app/src/components/Mainpage/Navbar/PcMenu.js
@@ -8,7 +8,7 @@ const PcMenu = () => {
             className={`hidden dt:flex justify-center items-center bg-[#005648] h-[60px] border-[1px]
              border-b-white ${
                  scrollPosition
-                     ? "dt:fixed  dt:h-[60px] w-[90%] z-[555] top-0 left-[5%] bg-[#005648d7] ease-in-out duration-500"
+                     ? "dt:fixed  dt:h-[60px] w-[90%] z-[555] top-0 left-[5%] bg-[#005648d7] ease-in-out duration-700"
                      : ""
              }`}
         >
@@ -23,22 +23,22 @@ const PcMenu = () => {
                     <MdKeyboardDoubleArrowDown className="group-hover:rotate-180 self-center duration-200 ease-linear ml-[10px]"></MdKeyboardDoubleArrowDown>
                     <ul
                         className="absolute   
-                        duration-500 ease-linear scale-0 group-hover:scale-100 text-center  top-[2.4em] w-fit  p-[15px] z-[100] self-start"
+                        duration-400 ease-linear scale-0 group-hover:scale-100 text-center  top-[2.4em] w-fit  p-[15px] z-[100] self-start"
                     >
-                        <li className=" text-[16px] font-semibold hover:text-[20px] z-[100] translate-x-[-100%] p-[15px] bg-premierColor hover:bg-opacity-50   scale-0 group-hover:translate-x-[0]    duration-[0.5s] ease-in-out group-hover:scale-100 ">
+                        <li className=" text-[16px] font-semibold hover:text-[20px] z-[100] translate-x-[-100%] p-[15px] bg-premierColor hover:bg-opacity-50   scale-0 group-hover:translate-x-[0]    duration-[0.2s] ease-in-out group-hover:scale-100 ">
                             <Link
                                 to="/VisionCare"
                                 alt="VisionCare "
-                                className=" duration-[0.5s] ease-in-out "
+                                className=" duration-[0.2s] ease-in-out "
                             >
                                 Synvård
                             </Link>
                         </li>
-                        <li className=" text-[16px] font-semibold hover:text-[20px] z-[100] translate-x-[-100%] p-[15px] bg-premierColor hover:bg-opacity-50   scale-0 group-hover:translate-x-[0]    duration-[0.5s] ease-in-out group-hover:scale-100 ">
+                        <li className=" text-[16px] font-semibold hover:text-[20px] z-[100] translate-x-[-100%] p-[15px] bg-premierColor hover:bg-opacity-50   scale-0 group-hover:translate-x-[0]    duration-[0.2s] ease-in-out group-hover:scale-100 ">
                             <Link
                                 to="/About"
                                 alt="Kontaklinser "
-                                className=" duration-[0.5s] ease-in-out "
+                                className=" duration-[0.2s] ease-in-out "
                             >
                                 Vad gör en Optiker?
                             </Link>
@@ -52,7 +52,7 @@ const PcMenu = () => {
                     <MdKeyboardDoubleArrowDown className="group-hover:rotate-180 self-center duration-200 ease-linear ml-[10px] "></MdKeyboardDoubleArrowDown>
                     <ul
                         className=" absolute   
-                      duration-500 ease-linear scale-0 group-hover:scale-100  top-[2.5em] w-fit  p-[15px] z-[100]"
+                      duration-400 ease-linear scale-0 group-hover:scale-100  top-[2.5em] w-fit  p-[15px] z-[100]"
                     >
                         <li
                             className=" text-[16px] font-semibold hover:text-[20px] z-[100] translate-x-[-100%]
@@ -62,7 +62,7 @@ const PcMenu = () => {
                             <Link
                                 to="/Offer"
                                 alt="Kontaklinser "
-                                className=" duration-[0.5s] ease-in-out"
+                                className=" duration-[0.2s] ease-in-out"
                             >
                                 Kontaktlinser
                             </Link>
